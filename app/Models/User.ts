2c97657879f943797403a6af7e24805ea770616e
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-11-19 21:21:25 +0100

    feat: add soft delete

--- app/Models/User.ts
+++ app/Models/User.ts
@@ -1,9 +1,11 @@
 import { DateTime } from 'luxon'
 import { BaseModel, beforeSave, column, HasOne, hasOne } from '@ioc:Adonis/Lucid/Orm'
+import { compose } from '@ioc:Adonis/Core/Helpers'
+import { SoftDeletes } from '@ioc:Adonis/Addons/LucidSoftDeletes'
 import Hash from '@ioc:Adonis/Core/Hash'
 import Profile from './Profile'
 
-export default class User extends BaseModel {
+export default class User extends compose(BaseModel, SoftDeletes) {
   @column({ isPrimary: true })
   public id: number
 
