fbad06235b9a7939faff5f952815bf48060fa82f
Esteban S <esteban.soubiran@insa-cvl.fr>
2021-11-20 16:08:30 +0100

    fix(routes): update profile

--- start/routes.ts
+++ start/routes.ts
@@ -36,7 +36,7 @@ Route.group(() => {
 Route.group(() => {
   Route.delete('/users/:id', 'UsersController.destroy')
 
-  Route.get('/me', 'ProfilesController.me')
+  Route.get('/profiles/me', 'ProfilesController.me')
   Route.patch('/profiles/:id', 'ProfilesController.update')
 
   Route.resource('/posts', 'PostsController').apiOnly()
